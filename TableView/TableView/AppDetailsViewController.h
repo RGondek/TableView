f990b33ddd290aefe0441720da4c1224bce725f8
LeonardoBrunassi <leo.brunassi.apple@hotmail.com>
2015-02-25 16:53:43 -0300

    Details + segue

---
+++ TableView/TableView/AppDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AppDetailsViewController.h
+//  TableView
+//
+//  Created by Leonardo Rodrigues de Morais Brunassi on 25/02/15.
+//  Copyright (c) 2015 Ckode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDetailsViewController : UIViewController
+
+@end
---
+++ TableView/TableView/AppDetailsViewController.m
@@ -0,0 +1,37 @@
+//
+//  AppDetailsViewController.m
+//  TableView
+//
+//  Created by Leonardo Rodrigues de Morais Brunassi on 25/02/15.
+//  Copyright (c) 2015 Ckode. All rights reserved.
+//
+
+#import "AppDetailsViewController.h"
+
+@interface AppDetailsViewController ()
+
+@end
+
+@implementation AppDetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
