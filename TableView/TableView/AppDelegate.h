d0fcd96363c7daf514e26bced02fb8f44440299a
Rubens <rubens.gondek@gmail.com>
2015-02-25 14:48:05 -0300

    Commit inicial

---
+++ TableView/TableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableView
+//
+//  Created by Rubens Gondek on 2/25/15.
+//  Copyright (c) 2015 Ckode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
